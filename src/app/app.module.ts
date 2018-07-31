5bf12b7159d9eb1ed41ce6c964780221693b83a4
Abilean <33487190+abilean@users.noreply.github.com>
2018-07-31 07:37:13 -0400

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { MyCustomMaterialModule } from './myCustomMaterialModule';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MyCustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
